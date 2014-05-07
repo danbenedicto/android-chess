1ef2f416146a21d44ebea92705b37c17e5448e7f
Dan Benedicto <danbenedicto@gmail.com>
2014-05-07 18:03:27 -0400

    implement goForward() and goBackward() in GamePlayer.java

--- src/controller/GamePlayer.java
+++ src/controller/GamePlayer.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.ChessPiece;
 import model.Game;
 import model.Move;
 import view.AndroidView;
@@ -14,6 +15,7 @@ public class GamePlayer {
 	public GamePlayer(Game game, AndroidView view){
 		this.game = game;
 		this.view = view;
+		this.currentFrame = 0;
 	}
 	
 	public void goForward(){
@@ -21,9 +23,53 @@ public class GamePlayer {
 		
 		Move move = game.moves.get(currentFrame++);
 		
-		if (move.type == Move.Type.NORMAL) {
-			
+		if (move.capture != null){
+			move.capture.loc.chessPiece = null;
+			move.capture.loc = null;
 		}
+		move.source.chessPiece = null;
+		move.chessPiece.loc = move.destination;
+		move.destination.chessPiece = move.chessPiece;
+		
+		if (move.type == Move.Type.CASTLE){		
+			ChessPiece rook = game.board[move.destination.x == 6 ? 7 : 0][move.source.y].chessPiece;
+			rook.loc.chessPiece = null;
+			rook.loc = game.board[move.destination.x == 6 ? 5 : 2][move.source.y];
+			rook.loc.chessPiece = rook;
+		}
+		
+		view.printBoard();
+	}
+	
+	public void goBackward(){
+		if (!canGoBackward()) return;
+		
+		Move move = game.moves.get(currentFrame--);
+		
+		if (move.type == Move.Type.NORMAL) {
+			move.chessPiece.loc = move.source;
+			move.source.chessPiece = move.chessPiece;
+			move.destination.chessPiece = move.capture;
+			if (move.capture != null){
+				move.capture.loc = move.destination;
+			}
+		} else if (move.type == Move.Type.CASTLE){
+			move.chessPiece.loc = move.source;
+			move.source.chessPiece = move.chessPiece;
+			
+			ChessPiece rook = game.board[move.destination.x == 6 ? 5 : 2][move.source.y].chessPiece;
+			rook.loc.chessPiece = null;
+			rook.loc = game.board[move.destination.x == 6 ? 7 : 0][move.source.y];
+			rook.loc.chessPiece = rook;
+		} else if (move.type == Move.Type.ENPASSANT){
+			move.chessPiece.loc = move.source;
+			move.source.chessPiece = move.chessPiece;
+			
+			move.capture.loc = game.board[move.destination.x][move.source.y];
+			move.capture.loc.chessPiece = move.capture;
+		}
+		
+		view.printBoard();
 	}
 	
 	public boolean canGoForward(){
