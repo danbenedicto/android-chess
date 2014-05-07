10eaf943692906735641c3ac51de431fda69a09e
MichaelMurray <devnull@localhost>
2014-05-07 18:09:47 -0500

    fix

---
+++ src/com/example/hellochess/MainPageActivity.java
@@ -0,0 +1,67 @@
+package com.example.hellochess;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainPageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_page);
+
+		playButton();
+		recordButton();
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_page, menu);
+		return true;
+	}
+	public void playButton(){
+		Button message = (Button) findViewById(R.id.button1);
+		message.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainPageActivity.this,MainActivity.class));
+				
+			}
+		});
+		
+	}
+	public void recordButton(){
+		Button message = (Button) findViewById(R.id.button2);
+		message.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainPageActivity.this,RecordActivity.class));
+				
+			}
+		});
+		
+	}
+	public void replayButton(){
+		Button message = (Button) findViewById(R.id.button3);
+		message.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainPageActivity.this,PlaybackActivity.class));
+				
+			}
+		});
+		
+	}
+
+
+}
