4711eab051e4fa8f85411730198e3efeb914d5f3
MichaelMurray <devnull@localhost>
2014-05-07 20:42:04 -0500

    cool stuff

--- src/com/example/hellochess/MainActivity.java
+++ src/com/example/hellochess/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.hellochess;
 
 import model.Game;
 import view.AndroidView;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -10,7 +13,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.Toast;
 import controller.Controller;
 
 public class MainActivity extends ActionBarActivity {
@@ -32,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
 		this.view.controller = this.controller;
 		
 		view.printBoard();
+		ressignButton();
+		drawButton();
 		
 	}
 
@@ -74,6 +81,86 @@ public class MainActivity extends ActionBarActivity {
 	
 	public void undoMove(View v){
 		controller.undoMove();
+	}
+	public void drawButton(){
+		Button message = (Button) findViewById(R.id.button2);
+		message.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				CharSequence text="Do you want to draw?";
+				Context context = getApplicationContext();
+				int duration = Toast.LENGTH_LONG;
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();	
+				AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+			    builder.setTitle("Confirm");
+			    builder.setMessage("Are you sure?");
+
+			    builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+
+			        public void onClick(DialogInterface dialog, int which) {
+			        	GridLayout chessGrid = (GridLayout) findViewById(R.id.BoardGridLayout);
+						chessGrid.removeAllViews();	MainActivity.this.game = new Game();
+						MainActivity.this.view = new AndroidView(MainActivity.this, null, game, chessGrid);
+					
+						MainActivity.this.controller = new Controller(game, MainActivity.this.view);
+						MainActivity.this.view.controller = MainActivity.this.controller;
+						view.printBoard();
+			        	
+
+			            dialog.dismiss();
+			        }
+
+			    });
+
+			    builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+
+			        @Override
+			        public void onClick(DialogInterface dialog, int which) {
+			            // Do nothing
+			            dialog.dismiss();
+			        }
+			    });
+
+			    AlertDialog alert = builder.create();
+			    alert.show();
+			}
+		});
+		
+	}
+	public void ressignButton(){
+		Button message = (Button) findViewById(R.id.button3);
+		message.setOnClickListener(new View.OnClickListener() {
+		
+
+			@Override
+			public void onClick(View v) {
+				CharSequence text;
+				Context context = getApplicationContext();
+				if(game.currentPlayer.toString().equals("w")){
+					text = "White resigns!";
+				}else{
+					text ="Black Resigns!";
+				}
+				int duration = Toast.LENGTH_LONG;
+				Toast toast = Toast.makeText(context, text, duration);
+				toast.show();
+				GridLayout chessGrid = (GridLayout) findViewById(R.id.BoardGridLayout);
+				chessGrid.removeAllViews();	MainActivity.this.game = new Game();
+				MainActivity.this.view = new AndroidView(MainActivity.this, null, game, chessGrid);
+			
+				MainActivity.this.controller = new Controller(game, MainActivity.this.view);
+				MainActivity.this.view.controller = MainActivity.this.controller;
+				view.printBoard();
+				
+
+			}
+			
+		}
+		);
+
 	}
 
 }
